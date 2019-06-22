cd4c852c7b5d44c6edad020f8487c4b6bf3a25d0
Okera Bishop <obishop25@yahoo.com>
2019-06-22 10:32:36 -0500

    completed recipe file

---
+++ script.js
@@ -0,0 +1,13 @@
+function marinaraSauce(cookName){
+    two="2"
+    oneFourth = "1/4"
+    
+    console.log("Hey," + cookName + "! This is my marinara sauce recipe!")
+    console.log("heat a medium-large saucepan over medium heat")
+    console.log("Add " + two + "2 tspns of oil and 5 garlic cloves, cook until golden, about" + two + "minutes")
+    console.log("Add" + oneFourth + "cup water," + two + "cans of crushed tomatoes, and salt and season with black pepper to taste")
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the suace is heated, about 10 minutes")
+    console.log("Stir in " + oneFourth + "cup roughly chopped fresh basil, salt and pepper as needed.")
+    console.log("This is a great recipe. Everyone LOVES IT")
+}
+marinaraSauce("Adam");
